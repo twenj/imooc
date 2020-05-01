4073dc7287e6e4df9dc8e1142a8f5ec2562c25f7
tangwenjing <twenj1992@163.com>
2020-05-01 18:33:13 +0800

    #

--- ObjectProj/src/com/imooc/animal/Cat.java
+++ ObjectProj/src/com/imooc/animal/Cat.java
@@ -6,10 +6,18 @@ package com.imooc.animal;
  */
 public class Cat {
 	// 成员属性：昵称、年龄、体重、品种
-	String name;
-	int month;
-	double weight;
-	String species;
+	private String name;
+	private int month;
+	private double weight;
+	private String species;
+	
+	public static int price;
+	{
+		System.out.println("我是构造代码块");
+	}//构造代码块，会在构造方法前执行
+	static {
+		System.out.println("我是静态代码块");
+	}// 静态代码块
 	public Cat () {
 		System.out.println("我是宠物猫");
 	}
@@ -25,9 +33,36 @@ public class Cat {
 		this.species = species;
 	}
 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public String getSpecies() {
+		return species;
+	}
+	public void setSpecies(String species) {
+		this.species = species;
+	}
 	// 成员方法：跑动、吃东西
 	// 跑动的方法
 	public void run() {
+		{
+			System.out.println("我是普通代码块");
+		}// 普通代码块
 		System.out.println("小猫快跑");
 	}
 	public void run(String name) {
--- ObjectProj/src/com/imooc/test/Test.java
+++ ObjectProj/src/com/imooc/test/Test.java
@@ -4,6 +4,24 @@ import com.imooc.animal.Cat;
 
 public class Test {
 	public static void main(String[] args) {
-		Cat cat = new Cat();	
+		Cat one = new Cat();
+//		one.run();
+		Cat two = new Cat();
+//		one.setName("花花");
+//		one.setMonth(2);
+//		one.setWeight(1000);
+//		one.setSpecies("英国短毛猫");
+//		one.price = 2000;
+//		Cat.price = 3000;
+		
+//		Cat two = new Cat();
+//		two.setName("凡凡");
+//		two.setMonth(1);
+//		two.setWeight(500);
+//		two.setSpecies("中华田园毛");
+//		two.price = 150;
+		
+//		System.out.println(one.getName() + "的售价" + one.price);
+//		System.out.println(two.getName() + "的售价" + two.price);
 	}
 }
